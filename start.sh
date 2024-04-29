a9744190e1d0e378bf2ef81417dd16294a2a5e49
Eraste <e.kouakou@omconsulting-group.com>
2024-04-29 17:58:23 +0000

    maj structure

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # run laravel app
-gnome-terminal -- bash -c "cd server/ && php artisan serve"
+gnome-terminal -- bash -c "cd ./server/ && php artisan serve"
 
-# run node js app
-gnome-terminal -- bash -c "cd client_nextjs/ && npm run dev"
+run node js app
+gnome-terminal -- bash -c "cd ./client_nextjs/ && npm run dev"
