8d02a84c1a6bfa264ae2292400b5eff2557fe54d
Eraste <e.kouakou@omconsulting-group.com>
2024-05-28 09:50:56 +0000

    maj

--- client_nextjs/src/containers/Product/Single/Single.tsx
+++ client_nextjs/src/containers/Product/Single/Single.tsx
@@ -14,6 +14,7 @@ import { fetchSingleProperties } from "app/axios/api.action";
 import { useParams } from "react-router-dom";
 import SingleBreadcrumb from "containers/PageSingle/SingleBreadcrumb";
 import SingleNotFound from "./SingleNotFound";
+import SingleImage from "containers/PageSingle/SingleImage";
 
 export interface SingleProps {
 	className?: string;
@@ -29,7 +30,7 @@ const Single: FC<SingleProps> = ({ className = "" }) => {
 	const dispatch = useAppDispatch();
 	const { slug } = useParams<{ slug: string }>();
 
-	const post = useAppSelector(PropertyAction.data);
+	const single = useAppSelector(PropertyAction.data)?.single;
 	const [isOpen, setIsOpen] = useState(false);
 	const [openFocusIndex, setOpenFocusIndex] = useState(0);
 	const [requestAttempted, setRequestAttempted] = useState(false);
@@ -43,11 +44,11 @@ const Single: FC<SingleProps> = ({ className = "" }) => {
 	}, []);
 
 	useEffect(() => {
-		if (!requestAttempted && post && !post.single && slug) {
+		if (!requestAttempted && !single && slug) {
 			dispatch(fetchSingleProperties({ slug: slug }));
 			setRequestAttempted(true);
 		}
-	}, [post, slug, dispatch, fetchSingleProperties, requestAttempted]);
+	}, [single, slug, dispatch, fetchSingleProperties, requestAttempted]);
 
 	const handleOpenModal = (index: number) => {
 		setIsOpen(true);
@@ -57,91 +58,57 @@ const Single: FC<SingleProps> = ({ className = "" }) => {
 
 	const PHOTOS = SINGLE_GALLERY.galleryImgs || [];
 
-	const galleryGrid = () => {
-		const imgs = post?.single?.images || [];
-		const firstImage = imgs.length > 0 && imgs[0].image;
-		const othersImages = imgs.length > 5 && imgs.slice(1, 5).map((img) => img.image);
-		const others = imgs.length > 5 && imgs.map((img) => img.image);
-
+	const gridTest = () => {
 		return (
-			<div className="relative grid grid-cols-3 sm:grid-cols-4 gap-2 my-10">
-				{firstImage && (
-					<div className="col-span-2 row-span-2 relative rounded-xl overflow-hidden cursor-pointer" onClick={() => handleOpenModal(0)}>
-						<NcImage containerClassName="absolute inset-0" className="object-cover w-full h-full rounded-xl" src={firstImage} />
-						<div className="absolute inset-0 bg-neutral-900 bg-opacity-20 opacity-0 hover:opacity-100 transition-opacity"></div>
+			<div className="container">
+				<div className="grid grid-cols-1 md:grid-cols-12 gap-4">
+					<div className="col-span-8 bg-gray-200 p-4">
+						<h2 className="text-lg font-bold mb-2">Colonne 1</h2>
+						<p>Contenu de la colonne 1</p>
 					</div>
-				)}
-
-				{othersImages &&
-					othersImages.map((item, index) => (
-						<div key={index} className={`relative rounded-xl overflow-hidden ${index >= 2 ? "hidden sm:block" : ""}`}>
-							<NcImage containerClassName="aspect-w-6 aspect-h-5" className="object-cover w-full h-full rounded-xl " src={item || ""} />
-
-							{/* OVERLAY */}
-							<div
-								className="absolute inset-0 bg-neutral-900 bg-opacity-20 opacity-0 hover:opacity-100 transition-opacity cursor-pointer"
-								onClick={() => handleOpenModal(index + 1)}
-							/>
-						</div>
-					))}
-
-				<div
-					className="absolute hidden md:flex md:items-center md:justify-center right-3 bottom-3 px-4 py-2 rounded-full bg-neutral-100 text-neutral-500 cursor-pointer hover:bg-neutral-200 z-10"
-					onClick={() => handleOpenModal(0)}
-				>
-					<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth={1.5}
-							d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-						/>
-					</svg>
-					<span className="ml-2 text-neutral-800 text-sm font-medium">Show all photos</span>
+					<div className="col-span-4 bg-gray-200 p-4 order-first md:order-last">
+						<h2 className="text-lg font-bold mb-2">Colonne 2</h2>
+						<p>Contenu de la colonne 2</p>
+					</div>
 				</div>
 			</div>
 		);
 	};
 
+	if (!single) {
+		<SingleNotFound />;
+	}
+
 	return (
 		<>
-			{post && !post.single ? (
-				<SingleNotFound />
-			) : (
-				<>
-					<div className="bg-gray-100 dark:bg-neutral-800">{post && post.single && <SingleBreadcrumb meta={post.single} />}</div>
+			<div className="bg-gray-100 dark:bg-neutral-800">{single && <SingleBreadcrumb meta={single} />}</div>
 
-					<div className={`nc-Single pt-8 lg:pt-16 ${className}`} data-nc-id="Single">
-						{/* SINGLE HEADER */}
-						<header className="container rounded-xl">
-							{/* <SingleHeader metaActionStyle="style2" hiddenDesc pageData={post?.single} /> */}
+			<div className={`nc-Single pt-8 lg:pt-16 ${className}`} data-nc-id="Single">
+				{/* SINGLE HEADER */}
+				<header className="container rounded-xl">
+					{/* <SingleHeader metaActionStyle="style2" hiddenDesc pageData={post?.single} /> */}
 
-							{galleryGrid()}
-						</header>
+					{single && <SingleImage meta={single} handleOpenModal={handleOpenModal} />}
+				</header>
 
-						{/* MODAL PHOTOS */}
-						<ModalPhotos
-							imgs={post?.single?.images.map((item) => item.image) || []}
-							isOpen={isOpen}
-							onClose={handleCloseModal}
-							initFocus={openFocusIndex}
-						/>
+				{/* MODAL PHOTOS */}
+				{single && (
+					<ModalPhotos imgs={single.images.map((item) => item.image) || []} isOpen={isOpen} onClose={handleCloseModal} initFocus={openFocusIndex} />
+				)}
 
-						{/* FEATURED IMAGE */}
-						<div className=""></div>
-						{/* SINGLE MAIN CONTENT */}
+				{/* FEATURED IMAGE */}
+				<div className=""></div>
+				{/* SINGLE MAIN CONTENT */}
 
-						{post && post.single && (
-							<div className="container">
-								<SingleContent data={post?.single} />
-							</div>
-						)}
-
-						{/* RELATED POSTS */}
-						<SingleRelatedPosts />
+				{single && (
+					<div className="container">
+						<SingleContent data={single} />
 					</div>
-				</>
-			)}
+				)}
+
+				{/* RELATED POSTS */}
+				<SingleRelatedPosts />
+			</div>
 		</>
 	);
 };
